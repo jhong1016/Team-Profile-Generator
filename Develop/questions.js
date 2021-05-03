8802d80df260aab50fc42a54a4b0767c53756465
Jin Hong <jnhg1016@gmail.com>
2021-05-03 23:59:20 +1000

    questions.js created to shorten index.js and const variable for question.js added to index.js

---
+++ Develop/questions.js
@@ -0,0 +1,169 @@
+const managerQuestions = [
+    {
+        type: 'input',
+        message: "This application will generate an HTML page for a software engineering team. A software engineering team usually consists of a Manager and any number of engineers and interns. First, what is your Manager's name?",
+        name: 'mgrName',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid name is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is your manager's employee ID?",
+        name: 'mgrId',
+        default: '101',
+        validate: function (answer) {
+            if (answer <= 0) {
+                return console.log("A valid employee ID is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is your manager's email address?",
+        name: 'mgrEmail',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid email address is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is your manager's office number?",
+        name: 'mgrOffice',
+        default: '414',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid office number is required.");
+            }
+            return true;
+        }
+    },
+];
+
+const confirmEmployee = [
+    {
+        type: 'confirm',
+        message: "Would you like to add another team member?",
+        name: 'confirmEmp',
+    }
+];
+
+const employeeType = [
+    {
+        type: 'list',
+        message: "Would you like to add an Engineer or Intern?",
+        choices: ['Engineer', 'Intern'],
+        name: 'empRole',
+    }
+];
+
+const engineerQuestions = [
+    {
+        type: 'input',
+        message: "What is your Engineer's name?",
+        name: 'engName',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid name is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is your Engineer's employee ID?",
+        name: 'engId',
+        default: '212',
+        validate: function (answer) {
+            if (answer <= 0) {
+                return console.log("A valid employee ID is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is your Engineer's email address?",
+        name: 'engEmail',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid email address is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is your Engineer's GitHub?",
+        name: 'engGithub',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid GitHub is required.");
+            }
+            return true;
+        }
+    }
+];
+
+const internQuestions = [
+    {
+        type: 'input',
+        message: "What is your Intern's name?",
+        name: 'internName',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid name is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is your Intern's employee ID?",
+        name: 'internId',
+        default: '616',
+        validate: function (answer) {
+            if (answer <= 0) {
+                return console.log("A valid employee ID is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is your Intern's email address?",
+        name: 'internEmail',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid email address is required.");
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        message: "What is the name of your Intern's university?",
+        name: 'internSchool',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("A valid school is required.");
+            }
+            return true;
+        }
+    }
+];
+
+module.exports = {
+    manager: managerQuestions,
+    create: confirmEmployee,
+    employee: employeeType,
+    engineer: engineerQuestions,
+    intern: internQuestions
+};
