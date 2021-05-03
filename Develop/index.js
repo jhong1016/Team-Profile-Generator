558915cbf318ef8024627b680cfcac3eaa618b69
Jin Hong <jnhg1016@gmail.com>
2021-05-03 22:47:44 +1000

    const variables added to index.js

---
+++ Develop/index.js
@@ -0,0 +1,9 @@
+// External packages
+const fs = require("fs");
+const inquirer = require("inquirer");
+const path = require("path");
+
+// Internal modules
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
