2e2fea9e99a23ccc3020145e948d1dfb60ff2d39
Jin Hong <jnhg1016@gmail.com>
2021-05-03 23:30:32 +1000

    const variable to render intern html added

--- Develop/lib/htmlRenderer.js
+++ Develop/lib/htmlRenderer.js
@@ -44,5 +44,14 @@ const renderEngineer = engineer => {
     return template;
 };
 
+const renderIntern = intern => {
+    let template = fs.readFileSync(path.resolve(templatesDir, "intern.html"), "utf8");
+    template = replacePlaceholders(template, "name", intern.getName());
+    template = replacePlaceholders(template, "role", intern.getRole());
+    template = replacePlaceholders(template, "email", intern.getEmail());
+    template = replacePlaceholders(template, "id", intern.getId());
+    template = replacePlaceholders(template, "school", intern.getSchool());
+    return template;
+};
   
 module.exports = render;
